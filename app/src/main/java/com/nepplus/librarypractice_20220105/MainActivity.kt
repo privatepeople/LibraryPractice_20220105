36f56abecd1a6740562f0254e9212b48bcb04261
Lee Seung Hun <negrat@naver.com>
2022-01-05 10:19:31 +0900

    www.jitpack.io 사이트에서도 라이브러리를 찾아보도록 세팅 변경

--- app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.nepplus.librarypractice_20220105
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imgProfilePhoto.setOnClickListener {
+
+//              사진 크게 보기 화면으로 진입. (Intent - 추가 액티비티 필요)
+
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/nepplus/librarypractice_20220105/ViewPhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.librarypractice_20220105
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+    }
+}
