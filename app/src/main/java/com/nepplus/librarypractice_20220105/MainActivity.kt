b5328b3fcd0d6ddedf46f2993ae897e0e1aa6b41
Lee Seung Hun <negrat@naver.com>
2022-01-05 10:56:44 +0900

    Glide 라이브러리 설치 / 코틀린코드 작성     - 이미지가 아직 안불러와짐

--- app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.librarypractice_20220105
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+//      Glide 이용 => 웹의 이미지를 -> imREcent에 반영하기
+        Glide.with(this).load("https://d3n24gmmpz5ort.cloudfront.net/2021/02/1edea7cf8a24b3a46558fccd4cc61ea4/%EC%8D%B8%EB%84%A4%EC%9D%BC.jpg")
 
     }
 }
